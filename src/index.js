7259b9f4e8b14fb101995e147b98dd8d6204db97
MSaidG <msgulberk@gmail.com>
2024-10-04 21:11:28 +0300

    Make basic template of the ui

--- src/index.js
+++ src/index.js
@@ -27,8 +27,6 @@ import wind from "./icons/wind.svg"
 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", 
   "Thursday", "Friday", "Saturday"]
-const img = document.getElementById('tornado')
-img.src = tornado
 
 const catImg = document.getElementById('cat');
 const p = document.getElementById('weather');
@@ -74,9 +72,8 @@ function getWeather(url)
   })
   .then(response => {
     console.log(response)
-    p.textContent = response.days[0].conditions + "\n" + response.resolvedAddress
-    catIcon = response.days[0].catIcon 
-    parseWeatherInfoData(response)
+    parseTodayWeatherInfoData(response)
+    parseTodayWeatherData(response)
     parseWeatherDailyData(response)
   })
   .catch(err => {
@@ -85,9 +82,6 @@ function getWeather(url)
 }
 getWeather(defaultUrlWeather)
 
-const button = document.getElementById("get")
-button.textContent = "Get Cat"
-button.addEventListener("click", () => getTheCat())
 
 const input = document.getElementById("location")
 input.addEventListener('keypress', (e) => onInputEnter(e));
@@ -115,31 +109,60 @@ const feelslike = document.getElementsByClassName('feelslike')
 const humidity = document.getElementsByClassName('humidity')
 const dew = document.getElementsByClassName('dew')
 const windspeed = document.getElementsByClassName('windspeed')
-const winddir = document.getElementsByClassName('winddir')
 const pressure = document.getElementsByClassName('pressure')
 const visibility = document.getElementsByClassName('visibility')
 const cloudcover = document.getElementsByClassName('cloudcover')
 const solarradiation = document.getElementsByClassName('solarradiation')
 const solarenergy = document.getElementsByClassName('solarenergy')
 const uvindex = document.getElementsByClassName('uvindex')
-const conditions = document.getElementsByClassName('conditions')
 const icon = document.getElementsByClassName('icon')
 const sunrise = document.getElementsByClassName('sunrise')
 const sunset = document.getElementsByClassName('sunset')
 const moonphase = document.getElementsByClassName('moonphase')
 const iconImg = document.getElementsByClassName('icon-img')
+const dir = document.getElementsByClassName('dir')
 
-function parseWeatherInfoData(response)
+
+
+function convertDDToDMS(D){
+  return [0|D, '° ', 0|(D=(D<0?-D:D)+1e-4)%1*60, "' ", 0|D*60%1*60, '"'].join('');
+}
+
+
+function parseTodayWeatherInfoData(response)
 {
-  latitude.textContent = response.latitude
-  longitude.textContent = response.longitude
+  if (response.longitude > 0)
+  {
+    longitude.textContent = `${convertDDToDMS(response.longitude)} °E)` 
+  }
+  else
+  {
+    longitude.textContent = `${convertDDToDMS(response.longitude)} °W)`
+  }
+  if (response.latitude > 0)
+  {
+    latitude.textContent = `(${convertDDToDMS(response.latitude)} °N ,` 
+  }
+  else
+  {
+    latitude.textContent = `(${convertDDToDMS(response.latitude)} °S ,` 
+  }
   resolvedAddress.textContent = response.resolvedAddress
-  timezone.textContent = response.timezone + "  +" + response.tzoffset
+  if (response.tzoffset === 0)
+  {
+    timezone.textContent = response.timezone + "  (UTC+00)"
+  }
+  else if (response.tzoffset < 0)
+  {
+    let offset = Math.abs(response.tzoffset).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
+    timezone.textContent = response.timezone + `  (UTC −${offset})`
+  }
+  else
+  {
+    let offset = response.tzoffset.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
+    timezone.textContent = response.timezone + `  (UTC +${offset})`
+  }
   description.textContent = response.description
-  latitude.textContent = response.latitude
-  latitude.textContent = response.latitude
-  latitude.textContent = response.latitude
-  latitude.textContent = response.latitude
 }
 
 function getCurrentDay()
@@ -154,7 +177,7 @@ function getCurrentMonth()
   return month
 }
 
-function parseWeatherDailyData(response)
+function parseTodayWeatherData(response)
 {
   datetime[0].textContent = response.days[0].datetime
   day[0].textContent = getCurrentDay() 
@@ -163,19 +186,42 @@ function parseWeatherDailyData(response)
   humidity[0].textContent = response.days[0].humidity + " %"
   dew[0].textContent = response.days[0].dew + " °C"
   windspeed[0].textContent = response.days[0].windspeed + " kph"
-  winddir[0].textContent = response.days[0].winddir + " degrees"
+  dir[0].setAttribute("transform", `translate(20,20)
+     rotate(${response.days[0].winddir})`);
   pressure[0].textContent = response.days[0].pressure + " mb"
   visibility[0].textContent = response.days[0].visibility + " km"
   cloudcover[0].textContent = response.days[0].cloudcover + " % "
   solarradiation[0].textContent = response.days[0].solarradiation + " W/m2"
   solarenergy[0].textContent = response.days[0].solarenergy + " MJ/m2"
   uvindex[0].textContent = response.days[0].uvindex
-  conditions[0].textContent = response.days[0].conditions
-  icon[0].textContent = response.days[0].icon
   selectWeatherIcon(response, 0)
+  icon[0].textContent = response.days[0].icon.replace("-", " ")
+                  .split(' ')
+                  .map(word => word.charAt(0)
+                  .toUpperCase() + word.substring(1))
+                  .join(' ');
   sunrise[0].textContent = response.days[0].sunrise
   sunset[0].textContent = response.days[0].sunset
   moonphase[0].textContent = response.days[0].moonphase
+}
+
+function parseWeatherDailyData(response)
+{
+  for (let i = 1; i < temp.length; i++)
+  {
+    temp[i].textContent = response.days[i].temp + " °C"
+    selectWeatherIcon(response, i)
+    icon[i].textContent = response.days[i].icon.replace("-", " ")
+                    .split(' ')
+                    .map(word => word.charAt(0)
+                    .toUpperCase() + word.substring(1))
+                    .join(' ');
+    feelslike[i].textContent = response.days[i].feelslike + " °C"
+    windspeed[i].textContent = response.days[i].windspeed + " kph"
+    dir[i].setAttribute("transform", `translate(20,20)
+       rotate(${response.days[i].winddir})`);
+  }
+
 }
 
 function selectWeatherIcon(response, index)
